09ca5818c23538d0adcf5f10114c63c1cac52fa0
SBC4 <SBC4@SBC09>
2015-04-28 17:58:47 +0700

    belajar

---
+++ src/projekbaru/MDMJ.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekbaru;
+
+/**
+ *
+ * @author SBC4
+ */
+public class MDMJ {
+    
+}

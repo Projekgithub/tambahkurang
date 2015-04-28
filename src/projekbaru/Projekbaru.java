ce93ce235cbc1b1295f6551c92239f59bfedf29d
SBC4 <SBC4@SBC08>
2015-04-28 17:50:11 +0700

    Testing

---
+++ src/projekbaru/Projekbaru.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekbaru;
+
+/**
+ *
+ * @author SBC4
+ */
+public class Projekbaru {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 8;
+        int b = 2;
+        int c = a + b;
+        int d = a - b;
+        
+        System.out.println("addition result = "+c);
+        System.out.println("simplication result = "+d);
+    }
+}

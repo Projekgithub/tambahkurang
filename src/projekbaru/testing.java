a17b09accdcdf11c33772e654b3306a953502e08
SBC4 <SBC4@SBC09>
2015-04-28 18:18:25 +0700

    belajar

---
+++ src/projekbaru/testing.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekbaru;
+
+/**
+ *
+ * @author SBC4
+ */
+public class testing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int f=2;
+        int g=4;
+        int a=f+g;
+        int b=f-g;
+        System.out.println("tambah= "+c);
+        System.out.println("kurang= "+d);
+    }
+}
+
